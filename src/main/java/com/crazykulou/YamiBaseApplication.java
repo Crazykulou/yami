1e267cc3251f1c155c69185fce5457da217a9b81
Crazykulou <wxwx5566@163.com>
2022-10-25 00:31:28 +0800

    iniT

---
+++ src/main/java/com/crazykulou/YamiBaseApplication.java
@@ -0,0 +1,13 @@
+package com.crazykulou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamiBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamiBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crazykulou/YamiBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazykulou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamiBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
